bb6ecccbb733e3529978975639cd56c7f98754ee
Josh <joshconstantinelim@gmail.com>
2024-10-28 19:22:35 +1100

    wk8

--- wk8/reachable.c
+++ wk8/reachable.c
@@ -5,7 +5,30 @@
 #include "Graph.h"
 #include "Set.h"
 
-Set reachable(Graph g, Vertex src) {
-    // TODO
-    return NULL;
+void dfs(Graph g, Vertex v, Set visited) {
+    SetInsert(visited, v);
+
+    for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+        if (!SetContains(visited, w) && GraphIsAdjacent(g, v, w)) {
+            dfs(g, w, visited);
+        }
+    }
 }
+
+Set reachable(Graph g, Vertex src) {
+    Set visited = SetNew();
+
+    dfs(g, src, visited);
+
+    return visited;
+}
+
+// void dfs(Graph g, Vertex v, bool* visited) {
+//     visited[v] = true;
+
+//     for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+//         if (!visited[w] && GraphIsAdjacent(g, v, w)) {
+//             dfs(g, w, visited);
+//         }
+//     }
+// }
